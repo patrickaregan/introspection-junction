266852b6217a4b42df45ead3617c88e5d72b01ac
patrickaregan <patrickaregan@gmail.com>
2022-05-01 16:55:21 -0500

    refactor thought to add reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,29 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
+
 const ThoughtSchema = new Schema (
     {
         thoughtText: {
@@ -17,7 +40,8 @@ const ThoughtSchema = new Schema (
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-        }
+        },
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
@@ -27,6 +51,11 @@ const ThoughtSchema = new Schema (
         id: false
     }
 )
+
+// get count of reactions
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
 
 const Thought = model('Thought', ThoughtSchema);
 
